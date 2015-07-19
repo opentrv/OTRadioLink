4b5aa1188b40228d0947a7443dff8ed17b839ecc
DamonHD <d@hd.org>
2015-07-19 23:11:50 +0100

    Capturing view of current start-up to be able to replicate better.

--- content/OTRadioLink/utility/OTRadioLink_OTRadioLink.cpp
+++ content/OTRadioLink/utility/OTRadioLink_OTRadioLink.cpp
@@ -18,8 +18,23 @@ Author(s) / Copyright (s): Damon Hart-Davis 2015
 
 #include "OTRadioLink_OTRadioLink.h"
 
+
+// Note start-up sequence as-was from V0p2_Main 2015/07/18.
+//  // Initialise the radio, if configured, ASAP because it can suck a lot of power until properly initialised.
+//  RFM22PowerOnInit(); // Becomes... RFM23B.preinit(NULL);
+//  // Check that the radio is correctly connected; panic if not...
+//  if(!RFM22CheckConnected()) { panic(); }
+//  // Configure the radio.
+//  RFM22RegisterBlockSetup(FHT8V_RFM22_Reg_Values);
+//  // Put the radio in low-power standby mode.
+//  RFM22ModeStandbyAndClearState();
+
+
+
 // Use namespaces to help avoid collisions.
 namespace OTRadioLink
     {
 
     }
+
+
