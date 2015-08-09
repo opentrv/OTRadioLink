30a17ef79d1918844f71a03baf4e1945a51c4aef
DamonHD <d@hd.org>
2015-08-09 08:45:48 +0100

    COH-45: improved comments

--- content/OTRadioLink/utility/OTRadioLink_OTRadioLink.h
+++ content/OTRadioLink/utility/OTRadioLink_OTRadioLink.h
@@ -95,6 +95,8 @@ namespace OTRadioLink
             // The message length is passed by reference and may be *reduced* by the filter if appropriate.
             // This routine must complete quickly and must not do things unsafe in an ISR context,
             // such as access to non-volatile state or operations such as EEPROM access on the ATmega.
+            // In extreme cases this can be used as a hook to trigger fast processing
+            // entirely in the ISR, skipping the usual processing flow.
             // TODO: should take RX channel parameter (different protocols may be in use on each channel).
             typedef bool quickFrameFilter_t(const volatile uint8_t *buf, volatile uint8_t &buflen);
 
