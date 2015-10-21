c7088be3626de30f5e946aea1968a78127426062
Denzo77 <Denzo77@users.noreply.github.com>
2015-10-21 13:54:39 +0100

    Todo-573: Software serial implementation

---
+++ dev/test/softSerialTest/softSerialTest.ino
@@ -0,0 +1,45 @@
+#include <OTSoftSerial.h>
+#include <OTV0p2Base.h>
+#include <SoftwareSerial.h>
+
+OTV0P2BASE::OTSoftSerial ser(7, 8);
+//SoftwareSerial ser(7,8);
+static const char sendStr1[] = "AT\n";
+
+static const uint16_t baud = 4800;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(baud);
+  ser.begin();  // defaults to 2400. This is what sim900 is currently set to.
+  Serial.println("Start Program");
+  Serial.print("START\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  uint8_t val[6];
+  memset(val, 0, sizeof(val));
+
+  
+  if(Serial.available() > 0) {
+    char input = Serial.read();
+    Serial.println(input);
+    if(input == 'a') {
+      Serial.println("Go");
+      uint8_t i = 5;
+      sendAT();
+
+      Serial.print(ser.read(val, sizeof(val)-1));
+      Serial.print("\t:");
+
+      Serial.println((char *)val);
+    }
+  }
+}
+
+void sendAT()
+{
+  ser.write(sendStr1, sizeof(sendStr1)-1);
+}
+
