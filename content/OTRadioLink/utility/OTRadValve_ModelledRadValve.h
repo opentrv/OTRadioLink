d7cd76faff8065b60740139137de6801ff7f01ef
DamonHD <d@hd.org>
2015-11-08 18:26:04 +0000

    TODO-593: inner state classes compile

--- content/OTRadioLink/utility/OTRadValve_ModelledRadValve.h
+++ content/OTRadioLink/utility/OTRadValve_ModelledRadValve.h
@@ -36,19 +36,13 @@ namespace OTRadValve
     {
 
 
-// Simple mean filter.
-// Find mean of group of ints where sum can be computed in an int without loss.
-// TODO: needs a unit test or three.
-template<size_t N> int smallIntMean(const int data[N])
-  {
-  // Extract mean.
-  // Assume values and sum will be nowhere near the limits.
-  int sum = 0;
-  for(int8_t i = N; --i >= 0; ) { sum += data[i]; }
-  // Compute rounded-up mean.
-  return((sum + (int)(N/2)) / (int)N); // Avoid accidental computation as unsigned...
-  }
-
+// Default maximum time to allow the boiler to run on to allow for lost call-for-heat transmissions etc.
+// Should be (much) greater than the gap between transmissions (eg ~2m for FHT8V/FS20).
+// Should be greater than the run-on time at the OpenTRV boiler unit and any further pump run-on time.
+// Valves may have to linger open at minimum of this plus maybe an extra minute or so for timing skew
+// for systems with poor/absent bypass to avoid overheating.
+// Having too high a linger time value may cause excessive temperature overshoot.
+static const uint8_t DEFAULT_MAX_RUN_ON_TIME_M = 5;
 
 // Delay in minutes after increasing flow before re-closing is allowed.
 // This is to avoid excessive seeking/noise in the presence of strong draughts for example.
@@ -97,6 +91,111 @@ struct ModelledRadValveInputState
   // Reference (room) temperature in C/16; must be set before each valve position recalc.
   // Proportional control is in the region where (refTempC16>>4) == targetTempC.
   int refTempC16;
+  };
+
+
+// All retained state for computing valve movement, eg containing time-based state.
+// Exposed to allow easier unit testing.
+// All initial values set by the constructor are sane.
+struct ModelledRadValveState
+  {
+  ModelledRadValveState() :
+    initialised(false),
+    isFiltering(false),
+    valveMoved(false),
+    cumulativeMovementPC(0),
+    valveTurndownCountdownM(0), valveTurnupCountdownM(0)
+    { }
+
+  // Perform per-minute tasks such as counter and filter updates then recompute valve position.
+  // The input state must be complete including target and reference temperatures
+  // before calling this including the first time whereupon some further lazy initialisation is done.
+  //   * valvePCOpenRef  current valve position UPDATED BY THIS ROUTINE, in range [0,100]
+  void tick(volatile uint8_t &valvePCOpenRef, const ModelledRadValveInputState &inputState);
+
+  // True once all deferred initialisation done during the first tick().
+  // This takes care of setting state that depends on run-time data
+  // such as real temperatures to propagate into all the filters.
+  bool initialised;
+
+  // If true then filtering is being applied to temperatures since they are fast-changing.
+  bool isFiltering;
+
+  // True if the computed valve position was changed by tick().
+  bool valveMoved;
+
+  // Cumulative valve movement count, as unsigned cumulative percent with rollover [0,8191].
+  // This is a useful as a measure of battery consumption (slewing the valve)
+  // and noise generated (and thus disturbance to humans) and of appropriate control damping.
+  //
+  // Keep as an unsigned 12-bit field (uint16_t x : 12) to ensure that
+  // the value doesn't wrap round to -ve value
+  // and can safely be sent/received in JSON by hosts with 16-bit signed ints,
+  // and the maximum number of decimal digits used in its representation is limited to 4
+  // and used efficiently (~80% use of the top digit).
+  //
+  // Daily allowance (in terms of battery/energy use) is assumed to be about 400% (DHD20141230),
+  // so this should hold many times that value to avoid ambiguity from missed/infrequent readings,
+  // especially given full slew (+100%) in nominally as little as 1 minute.
+  uint16_t cumulativeMovementPC : 12;
+
+  // Set non-zero when valve flow is constricted, and then counts down to zero.
+  // Some or all attempts to open the valve are deferred while this is non-zero
+  // to reduce valve hunting if there is string turbulence from the radiator
+  // or maybe draughts from open windows/doors
+  // causing measured temperatures to veer up and down.
+  // This attempts to reduce excessive valve noise and energy use
+  // and help to avoid boiler short-cycling.
+  uint8_t valveTurndownCountdownM;
+  // Mark flow as having been reduced.
+  void valveTurndown() { valveTurndownCountdownM = ANTISEEK_VALVE_REOPEN_DELAY_M; }
+  // If true then avoid turning up the heat yet.
+  bool dontTurnup() const { return(0 != valveTurndownCountdownM); }
+
+  // Set non-zero when valve flow is increased, and then counts down to zero.
+  // Some or all attempts to close the valve are deferred while this is non-zero
+  // to reduce valve hunting if there is string turbulence from the radiator
+  // or maybe draughts from open windows/doors
+  // causing measured temperatures to veer up and down.
+  // This attempts to reduce excessive valve noise and energy use
+  // and help to avoid boiler short-cycling.
+  uint8_t valveTurnupCountdownM;
+  // Mark flow as having been increased.
+  void valveTurnup() { valveTurnupCountdownM = ANTISEEK_VALVE_RECLOSE_DELAY_M; }
+  // If true then avoid turning down the heat yet.
+  bool dontTurndown() const { return(0 != valveTurnupCountdownM); }
+
+  // Length of filter memory in ticks; strictly positive.
+  // Must be at least 4, and may be more efficient at a power of 2.
+  static const size_t filterLength = 16;
+
+  // Previous unadjusted temperatures, 0 being the newest, and following ones successively older.
+  // These values have any target bias removed.
+  // Half the filter size times the tick() interval gives an approximate time constant.
+  // Note that full response time of a typical mechanical wax-based TRV is ~20mins.
+  int prevRawTempC16[filterLength];
+
+  // Get smoothed raw/unadjusted temperature from the most recent samples.
+  int getSmoothedRecent() const;
+
+  // get last change in temperature, +ve means rising.
+  int getRawDelta() const { return(prevRawTempC16[0] - prevRawTempC16[1]); }
+
+//  // Compute an estimate of rate/velocity of temperature change in C/16 per minute/tick.
+//  // A positive value indicates that temperature is rising.
+//  // Based on comparing the most recent smoothed value with an older smoothed value.
+//  int getVelocityC16PerTick();
+
+    // Computes a new valve position given supplied input state including the current valve position; [0,100].
+    // Uses no state other than that passed as the arguments (thus unit testable).
+    // FIXME: This supplied 'retained' state may be updated, nothing else is changed.
+    // Uses hysteresis and a proportional control and some other cleverness.
+    // Is always willing to turn off quickly, but on slowly (AKA "slow start" algorithm),
+    // and tries to eliminate unnecessary 'hunting' which makes noise and uses actuator energy.
+    // Nominally called at a regular rate, once per minute.
+    // All inputState values should be set to sensible values before starting.
+    // Usually called by tick() which does required state updates afterwards.
+    uint8_t computeRequiredTRVPercentOpen(const uint8_t currentValvePCOpen, const ModelledRadValveInputState &inputState) const;
   };
 
 
