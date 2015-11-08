d7ea359904aba39fb98eaf34c114dba561455493
DamonHD <d@hd.org>
2015-11-08 14:13:37 +0000

    TODO-593: copied down ModelledRadValveInputState

--- content/OTRadioLink/utility/OTRadValve_ModelledRadValve.cpp
+++ content/OTRadioLink/utility/OTRadValve_ModelledRadValve.cpp
@@ -20,8 +20,33 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2015
 
 #include "OTRadValve_ModelledRadValve.h"
 
+#include "utility/OTRadValve_AbstractRadValve.h"
+
 namespace OTRadValve
     {
+
+
+// Offset from raw temperature to get reference temperature in C/16.
+static const int8_t refTempOffsetC16 = 8;
+
+ModelledRadValveInputState::ModelledRadValveInputState(const int realTempC16) :
+    targetTempC(12 /* FROST */),
+    minPCOpen(OTRadValve::DEFAULT_VALVE_PC_MIN_REALLY_OPEN), maxPCOpen(100),
+    widenDeadband(false), glacial(false), hasEcoBias(false), inBakeMode(false)
+    { setReferenceTemperatures(realTempC16); }
+
+// Calculate reference temperature from real temperature.
+// Proportional temperature regulation is in a 1C band.
+// By default, for a given target XC the rad is off at (X+1)C so temperature oscillates around that point.
+// This routine shifts the reference point at which the rad is off to (X+0.5C)
+// ie to the middle of the specified degree, which is more intuitive,
+// and which may save a little energy if users target the specified temperatures.
+// Suggestion c/o GG ~2014/10 code, and generally less misleading anyway!
+void ModelledRadValveInputState::setReferenceTemperatures(const int currentTempC16)
+  {
+  const int referenceTempC16 = currentTempC16 + refTempOffsetC16; // TODO-386: push targeted temperature down by 0.5C to middle of degree.
+  refTempC16 = referenceTempC16;
+  }
 
 
     }
--- content/OTRadioLink/utility/OTRadValve_ModelledRadValve.h
+++ content/OTRadioLink/utility/OTRadValve_ModelledRadValve.h
@@ -36,6 +36,68 @@ namespace OTRadValve
     {
 
 
+// Simple mean filter.
+// Find mean of group of ints where sum can be computed in an int without loss.
+// TODO: needs a unit test or three.
+template<size_t N> int smallIntMean(const int data[N])
+  {
+  // Extract mean.
+  // Assume values and sum will be nowhere near the limits.
+  int sum = 0;
+  for(int8_t i = N; --i >= 0; ) { sum += data[i]; }
+  // Compute rounded-up mean.
+  return((sum + (int)(N/2)) / (int)N); // Avoid accidental computation as unsigned...
+  }
+
+
+// Delay in minutes after increasing flow before re-closing is allowed.
+// This is to avoid excessive seeking/noise in the presence of strong draughts for example.
+// Too large a value may cause significant temperature overshoots and possible energy wastage.
+static const uint8_t ANTISEEK_VALVE_RECLOSE_DELAY_M = 5;
+// Delay in minutes after restricting flow before re-opening is allowed.
+// This is to avoid excessive seeking/noise in the presence of strong draughts for example.
+// Too large a value may cause significant temperature undershoots and discomfort/annoyance.
+static const uint8_t ANTISEEK_VALVE_REOPEN_DELAY_M = (ANTISEEK_VALVE_RECLOSE_DELAY_M*2);
+
+// Typical heat turn-down response time; in minutes, strictly positive.
+static const uint8_t TURN_DOWN_RESPONSE_TIME_M = (ANTISEEK_VALVE_RECLOSE_DELAY_M + 3);
+
+// Assumed daily budget in cumulative (%) valve movement for battery-powered devices.
+static const uint16_t DEFAULT_MAX_CUMULATIVE_PC_DAILY_VALVE_MOVEMENT = 400;
+
+
+// All input state for computing valve movement.
+// Exposed to allow easier unit testing.
+// FIXME: add flag to indicate manual operation/override, so speedy response required (TODO-593).
+struct ModelledRadValveInputState
+  {
+  // All initial values set by the constructor are sane, but should not be relied on.
+  ModelledRadValveInputState(const int realTempC16);
+
+  // Calculate reference temperature from real temperature.
+  void setReferenceTemperatures(const int currentTempC16);
+
+  // Current target room temperature in C in range [MIN_TARGET_C,MAX_TARGET_C].
+  uint8_t targetTempC;
+  // Min % valve at which is considered to be actually open (allow the room to heat) [1,100].
+  uint8_t minPCOpen;
+  // Max % valve is allowed to be open [1,100].
+  uint8_t maxPCOpen;
+
+  // If true then allow a wider deadband (more temperature drift) to save energy and valve noise.
+  // This is a strong hint that the system can work less strenuously to hit, and stay on, target.
+  bool widenDeadband;
+  // True if in glacial mode.
+  bool glacial;
+  // True if an eco bias is to be applied.
+  bool hasEcoBias;
+  // True if in BAKE mode.
+  bool inBakeMode;
+
+  // Reference (room) temperature in C/16; must be set before each valve position recalc.
+  // Proportional control is in the region where (refTempC16>>4) == targetTempC.
+  int refTempC16;
+  };
 
 
     }
