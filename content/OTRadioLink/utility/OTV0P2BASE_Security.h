d6e8c348e686a7cbd1c001453049d0002ff4ef77
DamonHD <d@hd.org>
2015-11-14 20:50:17 +0000

    FHT8V: moving down

---
+++ content/OTRadioLink/utility/OTV0P2BASE_Security.h
@@ -0,0 +1,51 @@
+/*
+The OpenTRV project licenses this file to you
+under the Apache Licence, Version 2.0 (the "Licence");
+you may not use this file except in compliance
+with the Licence. You may obtain a copy of the Licence at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the Licence is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the Licence for the
+specific language governing permissions and limitations
+under the Licence.
+
+Author(s) / Copyright (s): Damon Hart-Davis 2015
+*/
+
+/*
+ Basic security support.
+ */
+
+#ifndef OTV0P2BASE_SECURITY_H
+#define OTV0P2BASE_SECURITY_H
+
+#include <stdint.h>
+
+
+namespace OTV0P2BASE
+{
+
+
+// Leaf node privacy level: how much to transmit about stats such as temperature and occupancy?
+// The greater numerically the value, the less data is sent, especially over an insecure channel.
+// Excess unencrypted stats may, for example, allow a clever burglar to work out when no one is home.
+// Note that even in the 'always' setting,
+// some TXes may be selectively skipped or censored for energy saving and security reasons
+// eg an additional 'never transmit occupancy' flag may be set locally.
+// The values correspond to levels and intermediate values not explicitly enumerated are allowed.
+// Lower values mean less that security is required.
+enum stats_TX_level
+  {
+    stTXalwaysAll = 0, // Always be prepared to transmit all stats.
+    stTXmostUnsec = 0x80, // Allow TX of all but most security-sensitive stats in plaintext, eg occupancy status.
+    stTXsecOnly = 0xfe, // Only transmit if the stats TX can be kept secure/encrypted.
+    stTXnever = 0xff, // Never transmit status info above the minimum necessary.
+  };
+
+
+}
+#endif
