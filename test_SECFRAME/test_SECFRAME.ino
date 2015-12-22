adca886703ab648b8754f767b321182dad7e387a
DamonHD <d@hd.org>
2015-12-22 15:00:14 +0000

    TODO-632: starting impl/tests

---
+++ test_SECFRAME/test_SECFRAME.ino
@@ -0,0 +1,162 @@
+/*
+The OpenTRV project licenses this file to you
+under the Apache Licence, Version 2.0 (the "Licence");
+you may not use this file except in compliance
+with the Licence. You may obtain a copy of the Licence at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the Licence is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the Licence for the
+specific language governing permissions and limitations
+under the Licence.
+
+Author(s) / Copyright (s): Damon Hart-Davis 2015
+*/
+
+/*
+ * EXTRA unit test routines for secureable-frame library code.
+ *
+ * Should all be runnable on any UNO-alike or emulator;
+ * does not need V0p2 boards or hardware.
+ *
+ * Some tests here may duplicate those in the core libraries,
+ * which is OK in order to double-check some core functionality.
+ */
+
+// Arduino libraries imported here (even for use in other .cpp files).
+
+#define UNIT_TESTS
+
+// Include the library under test.
+#include <OTV0p2Base.h>
+#include <OTRadValve.h>
+
+#if F_CPU == 1000000 // 1MHz CPU indicates V0p2 board.
+#define ON_V0P2_BOARD
+#endif
+
+void setup()
+  {
+#ifdef ON_V0P2_BOARD
+  // initialize serial communications at 4800 bps for typical use with V0p2 board.
+  Serial.begin(4800);
+#else
+  // initialize serial communications at 9600 bps for typical use with (eg) Arduino UNO.
+  Serial.begin(9600);
+#endif
+  }
+
+
+
+// Error exit from failed unit test, one int parameter and the failing line number to print...
+// Expects to terminate like panic() with flashing light can be detected by eye or in hardware if required.
+static void error(int err, int line)
+  {
+  for( ; ; )
+    {
+    Serial.print(F("***Test FAILED*** val="));
+    Serial.print(err, DEC);
+    Serial.print(F(" =0x"));
+    Serial.print(err, HEX);
+    if(0 != line)
+      {
+      Serial.print(F(" at line "));
+      Serial.print(line);
+      }
+    Serial.println();
+//    LED_HEATCALL_ON();
+//    tinyPause();
+//    LED_HEATCALL_OFF();
+//    sleepLowPowerMs(1000);
+    delay(1000);
+    }
+  }
+
+// Deal with common equality test.
+static inline void errorIfNotEqual(int expected, int actual, int line) { if(expected != actual) { error(actual, line); } }
+// Allowing a delta.
+static inline void errorIfNotEqual(int expected, int actual, int delta, int line) { if(abs(expected - actual) > delta) { error(actual, line); } }
+
+// Test expression and bucket out with error if false, else continue, including line number.
+// Macros allow __LINE__ to work correctly.
+#define AssertIsTrueWithErr(x, err) { if(!(x)) { error((err), __LINE__); } }
+#define AssertIsTrue(x) AssertIsTrueWithErr((x), 0)
+#define AssertIsEqual(expected, x) { errorIfNotEqual((expected), (x), __LINE__); }
+#define AssertIsEqualWithDelta(expected, x, delta) { errorIfNotEqual((expected), (x), (delta), __LINE__); }
+
+
+// Check that correct version of this library is under test.
+static void testLibVersion()
+  {
+  Serial.println("LibVersion");
+#if !(0 == ARDUINO_LIB_OTRADVALVE_VERSION_MAJOR) || !(9 == ARDUINO_LIB_OTRADVALVE_VERSION_MINOR)
+#error Wrong library version!
+#endif 
+  }
+
+// Check that correct versions of underlying libraries are in use.
+static void testLibVersions()
+  {
+  Serial.println("LibVersions");
+#if !(0 == ARDUINO_LIB_OTV0P2BASE_VERSION_MAJOR) || !(9 == ARDUINO_LIB_OTV0P2BASE_VERSION_MINOR)
+#error Wrong library version!
+#endif  
+  }
+
+
+
+// Test that direct abstract motor drive logic is sane.
+static void testNowt()
+  {
+  Serial.println("Nowt");
+//  AssertIsEqual(OTRadValve::CurrentSenseValveMotorDirect::init, csvmd1.getState());
+//  // Verify NOT marked as in normal run state immediately upon initialisation.
+//  AssertIsTrue(!csvmd1.isInNormalRunState());
+//  // Verify NOT marked as in error state immediately upon initialisation.
+//  AssertIsTrue(!csvmd1.isInErrorState());
+//  // Target % open must start off in a sensible state; fully-closed is good.
+//  AssertIsEqual(0, csvmd1.getTargetPC());
+
+  }
+
+
+
+
+// To be called from loop() instead of main code when running unit tests.
+// Tests generally flag an error and stop the test cycle with a call to panic() or error().
+void loop()
+  {
+  static int loopCount = 0;
+
+  // Allow the terminal console to be brought up.
+  for(int i = 3; i > 0; --i)
+    {
+    Serial.print(F("Tests starting... "));
+    Serial.print(i);
+    Serial.println();
+    delay(1000);
+    }
+  Serial.println();
+
+
+  // Run the tests, fastest / newest / most-fragile / most-interesting first...
+  testLibVersion();
+  testLibVersions();
+
+  testNowt();
+
+
+
+  // Announce successful loop completion and count.
+  ++loopCount;
+  Serial.println();
+  Serial.print(F("%%% All tests completed OK, round "));
+  Serial.print(loopCount);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  delay(2000);
+  }
