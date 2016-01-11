168d476b9b21407b6197fb0076a83be5ea193051
denzo <denzo@denzo-mint>
2016-01-11 19:10:30 +0000

    Initial framework for RN2483 class

---
+++ content/OTRadioLink/OTRN2483Link.h
@@ -0,0 +1,29 @@
+/*
+The OpenTRV project licenses this file to you
+under the Apache Licence, Version 2.0 (the "Licence");
+you may not use this file except in compliance
+with the Licence. You may obtain a copy of the Licence at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the Licence is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the Licence for the
+specific language governing permissions and limitations
+under the Licence.
+
+Author(s) / Copyright (s): Deniz Erbilgin 2016
+*/
+
+#ifndef ARDUINO_LIB_OTRN2483LINK_H_
+#define ARDUINO_LIB_OTRN2483LINK_H_
+
+#define ARDUINO_LIB_OTRN2483LINK_VERSION_MAJOR 0
+#define ARDUINO_LIB_OTRN2483LINK_VERSION_MINOR 0
+
+// RN2483 support.
+#include "utility/OTRN2483Link_OTRN2483Link.h"
+
+
+#endif /* ARDUINO_LIB_OTRN2483LINK_H_ */
