d78a1596efa56c755f0faa1d8c298fc3c8870369
DamonHD <d@hd.org>
2016-01-18 16:52:57 +0000

    D21FM: moved SHT21 temp/RH% sensor support down to base library.

---
+++ content/OTRadioLink/utility/OTV0P2BASE_SensorSHT21.cpp
@@ -0,0 +1,42 @@
+/*
+The OpenTRV project licenses this file to you
+under the Apache Licence, Version 2.0 (the "Licence");
+you may not use this file except in compliance
+with the Licence. You may obtain a copy of the Licence at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the Licence is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the Licence for the
+specific language governing permissions and limitations
+under the Licence.
+
+Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
+*/
+
+/*
+ SHT21 temperature and relative humidity detector.
+ */
+
+
+#include "OTV0P2BASE_SensorSHT21.h"
+
+//#include <Arduino.h>
+
+//#include "OTV0P2BASE_ADC.h"
+//#include "OTV0P2BASE_BasicPinAssignments.h"
+//#include "OTV0P2BASE_Entropy.h"
+//#include "OTV0P2BASE_PowerManagement.h"
+//#include "OTV0P2BASE_Sleep.h"
+
+
+namespace OTV0P2BASE
+{
+
+
+
+
+
+}
