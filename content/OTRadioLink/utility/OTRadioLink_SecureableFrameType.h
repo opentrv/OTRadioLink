5fc79fd79a3a12a676f62e270b41da98f7acb35c
DamonHD <d@hd.org>
2016-02-13 21:16:22 +0000

    TODO-721: indicating default secure frame ID length

--- content/OTRadioLink/utility/OTRadioLink_SecureableFrameType.h
+++ content/OTRadioLink/utility/OTRadioLink_SecureableFrameType.h
@@ -109,6 +109,11 @@ namespace OTRadioLink
     const static uint8_t ENC_BODY_SMALL_FIXED_PTEXT_MAX_SIZE =
         ENC_BODY_SMALL_FIXED_CTEXT_SIZE - 1;
 
+    // Standard length of ID to transmit in a secure frame (with body data).
+    // Long enough to make risk of non-unique prefixes very small even for large deployments.
+    // Short enough to produce an encrypted frame shorter than the maximum permitted.
+    const static uint8_t ENC_BODY_DEFAULT_ID_BYTES = 4;
+
     // Logical header for the secureable frame format.
     // Intended to be efficient to hold and work with in memory
     // and to convert to and from wire format.
