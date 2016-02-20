47137e07576b422b3424bb0517c22f7cb5fdafbb
DamonHD <d@hd.org>
2016-02-20 14:09:55 +0000

    TODO-721: improving msg count initialisation
    Making sure that msbs of ephemeral msg count
    stay zero at initialisation to preserve count life.

--- content/OTRadioLink/utility/OTRadioLink_SecureableFrameType.cpp
+++ content/OTRadioLink/utility/OTRadioLink_SecureableFrameType.cpp
@@ -633,11 +633,16 @@ bool getPrimarySecure6BytePersistentTXMessageCounter(uint8_t *const buf)
     static uint8_t ephemeral[3];
 
     // Temporary area for initialising ephemeral[] where needed.
-    uint8_t tmpE[sizeof(ephemeral) - 1];
+    uint8_t tmpE[sizeof(ephemeral)];
     if(!initialised)
         {
         for(uint8_t i = sizeof(tmpE); i-- > 0; )
-          { tmpE[i] = OTV0P2BASE::getSecureRandomByte(); } // Doesn't like being having interrupts off.
+          { tmpE[i] = OTV0P2BASE::getSecureRandomByte(); } // Doesn't like being called with interrupts off.
+        // Mask off top bits of top (most significant byte) to preserve most of the remaining counter life
+        // but allow ~20 bits ie a decent chunk of 1 million messages
+        // (maybe several years at a message every 4 minutes)
+        // before likely IV reuse even with absence/failure of the restart counter.
+        tmpE[0] = 0xf & (tmpE[0] ^ (tmpE[0] >> 4));
         }
 
     // Disable interrupts while adjusting counter and copying back to the caller.
@@ -648,11 +653,11 @@ bool getPrimarySecure6BytePersistentTXMessageCounter(uint8_t *const buf)
             // FIXME: increment persistent counter carefully FIXME FIXME
             // FIXME: fail if persistent count is all 0xff which will catch hitting ceiling and uninitialised EEPROM.
             // Fill lsbs of ephemeral part with entropy so as not to reduce lifetime significantly.
-            memcpy(ephemeral, tmpE, min(sizeof(tmpE), sizeof(ephemeral)));
+            memcpy(ephemeral+max(0,sizeof(ephemeral)-sizeof(tmpE)), tmpE, min(sizeof(tmpE), sizeof(ephemeral)));
             initialised = true;
             }
 
-        // FIXME: NOT FULLY IMPLEMENTED YET: IMPORTANT FOR SECURITY TO COMPLETE
+        // FIXME: NOT FULLY IMPLEMENTED YET: IMPORTANT FOR SECURITY TO COMPLETE THIS
 
         // FIXME: increment the counter including the persistent part where necessary.
         for(uint8_t i = sizeof(ephemeral); i-- > 0; )
