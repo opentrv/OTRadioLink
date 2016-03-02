b2fb11d7d08bc570b298dea86f4b41c2bda26e97
denzo <denzo@denzo-XPS-13-9343>
2016-03-02 16:16:13 +0000

    REV14 debug change

--- content/OTRadioLink/OTV0p2_CONFIG_REV14.h
+++ content/OTRadioLink/OTV0p2_CONFIG_REV14.h
@@ -165,6 +165,7 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 //#define RADIO_PRIMARY_RN2483 // Must be secondary to avoid sending preamble etc
 #define RADIO_SECONDARY_RN2483
 
+#define ENABLE_FREQUENT_STATS_TX
 // Define voice module
 //#define ENABLE_VOICE_SENSOR
 //#define ENABLE_OCCUPANCY_DETECTION_FROM_VOICE
