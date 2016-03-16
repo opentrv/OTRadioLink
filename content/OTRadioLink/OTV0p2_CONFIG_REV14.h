0d06389024786a46d59427f2f28a60f10a30b11c
DamonHD <d@hd.org>
2016-03-16 08:43:11 +0000

    TODO-861: WIP

--- content/OTRadioLink/OTV0p2_CONFIG_REV14.h
+++ content/OTRadioLink/OTV0p2_CONFIG_REV14.h
@@ -80,7 +80,7 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 //#define ENABLE_PRIMARY_TEMP_SENSOR_SHT21
 // IF DEFINED: enable use AVR's 'idle' mode to stop the CPU but leave I/O (eg Serial) running to save power.
 // DHD20150920: CURRENTLY NOT RECOMMENDED AS STILL SEEMS TO CAUSE SOME BOARDS TO CRASH.
-#define ENABLE_USE_OF_AVR_IDLE_MODE
+#undef ENABLE_USE_OF_AVR_IDLE_MODE
 
 // IF DEFINED: enable a 'null' radio module; without this unit is stand-alone.
 #define ENABLE_RADIO_NULL
@@ -93,7 +93,7 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 #define ENABLE_RADIO_SECONDARY_MODULE
 #define ENABLE_RADIO_RN2483   // Enable RN2483
 //#define RADIO_PRIMARY_RN2483 // Must be secondary to avoid sending preamble etc
-#define RADIO_SECONDARY_RN2483
+#define ENABLE_RADIO_SECONDARY_RN2483
 
 // Define voice module
 #define ENABLE_VOICE_SENSOR
@@ -140,8 +140,6 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 // IF DEFINED: enable use AVR's 'idle' mode to stop the CPU but leave I/O (eg Serial) running to save power.
 // DHD20150920: CURRENTLY NOT RECOMMENDED AS STILL SEEMS TO CAUSE SOME BOARDS TO CRASH.
 #define ENABLE_USE_OF_AVR_IDLE_MODE
-
-
 // IF DEFINED: use active-low LEARN button(s).  Needs ENABLE_SINGLETON_SCHEDULE.  ***
 #undef ENABLE_LEARN_BUTTON // OPTIONAL ON V0.09 PCB1
 // IF DEFINED: this unit supports CLI over the USB/serial connection, eg for run-time reconfig.
@@ -154,7 +152,6 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 #undef ENABLE_EXTENDED_CLI
 // IF DEFINED: minimise boot effort and energy eg for intermittently-powered energy-harvesting applications.  ***
 #undef ENABLE_MIN_ENERGY_BOOT
-
 // IF DEFINED: enable a 'null' radio module; without this unit is stand-alone.
 #define ENABLE_RADIO_NULL
 // Secondary radio
@@ -164,17 +161,82 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 #define ENABLE_RADIO_SECONDARY_MODULE
 #define ENABLE_RADIO_RN2483   // Enable RN2483
 //#define RADIO_PRIMARY_RN2483 // Must be secondary to avoid sending preamble etc
-#define RADIO_SECONDARY_RN2483
-
+#define ENABLE_RADIO_SECONDARY_RN2483
 //#define ENABLE_FREQUENT_STATS_TX
 #define ENABLE_JSON_STATS_LEN_CAP 49
 // Define voice module
 //#define ENABLE_VOICE_SENSOR
 //#define ENABLE_OCCUPANCY_DETECTION_FROM_VOICE
 //#define ENABLE_VOICE_STATS
-
 #endif
 
+#ifdef CONFIG_REV14_WORKSHOP // REV14 w/ light sensor and SHT21 for Launchpad final workshop.
+// Measured current consumption (no QM-1 or mobdet): 100-200 uA when serial shut and not attempting TX.
+// Revision REV14 of V0.2 board, Sensor unit with LoRa module and voice detector.
+// In this off-label mode being used as stats gatherers or simple hubs.
+// DE20160221: I may have made a mistake with my oscilloscope setup doing the earlier readings. Revised values below:
+// - QM-1 turned off, RN2483 in sleep mode, no mobdet:                  2-3 mA
+// - QM-1 turned on but not capturing, RN2483 in sleep mode, no mobdet: ~10 mA
+#define V0p2_REV 14
+// IF DEFINED: enable use of on-board SHT21 RH and temp sensor (in lieu of TMP112).
+#define ENABLE_PRIMARY_TEMP_SENSOR_SHT21
+// Using RoHS-compliant phototransistor in place of LDR.
+#define ENABLE_AMBIENT_LIGHT_SENSOR_PHOTOTRANS_TEPT4400
+// IF DEFINED: use the temperature-setting potentiometer/dial if present.
+#undef ENABLE_TEMP_POT_IF_PRESENT
+// IF DEFINED: basic FROST/WARM temperatures are settable.
+#undef ENABLE_SETTABLE_TARGET_TEMPERATURES
+// IF DEFINED: this unit will act as a thermostat controlling a local TRV (and calling for heat from the boiler), else is a sensor/hub unit.
+#undef ENABLE_LOCAL_TRV
+// IF DEFINED: this unit controls a valve, but provides slave valve control only.
+#undef ENABLE_SLAVE_TRV
+// IF DEFINED: this unit can act as boiler-control hub listening to remote thermostats, possibly in addition to controlling a local TRV.
+#undef ENABLE_BOILER_HUB // NO BOILER CODE
+// IF DEFINED: allow RX of stats frames.
+#undef ENABLE_STATS_RX
+// IF DEFINED: allow TX of stats frames.
+#define ENABLE_STATS_TX
+// IF DEFINED: always allow some kind of stats TX, whatever the privacy settings.
+#define ENABLE_ALWAYS_TX_ALL_STATS
+// IF DEFINED: allow JSON stats frames.
+#define ENABLE_JSON_OUTPUT
+// IF DEFINED: allow binary stats to be TXed.
+#undef ENABLE_BINARY_STATS_TX
+// IF DEFINED: enable support for FS20 carrier for RX of raw FS20 and piggybacked binary (non-JSON) stats.
+#undef ENABLE_FS20_NATIVE_AND_BINARY_STATS_RX
+//#define ENABLE_PRIMARY_TEMP_SENSOR_SHT21
+// IF DEFINED: enable use AVR's 'idle' mode to stop the CPU but leave I/O (eg Serial) running to save power.
+// DHD20150920: CURRENTLY NOT RECOMMENDED AS STILL SEEMS TO CAUSE SOME BOARDS TO CRASH.
+#define ENABLE_USE_OF_AVR_IDLE_MODE
+// IF DEFINED: use active-low LEARN button(s).  Needs ENABLE_SINGLETON_SCHEDULE.  ***
+#undef ENABLE_LEARN_BUTTON // OPTIONAL ON V0.09 PCB1
+// IF DEFINED: this unit supports CLI over the USB/serial connection, eg for run-time reconfig.
+#define ENABLE_CLI
+// IF DEFINED: enable a full OpenTRV CLI.
+#define ENABLE_FULL_OT_CLI
+// IF DEFINED: enable a full OpenTRV UI with normal LEDs etc. ***
+#undef ENABLE_FULL_OT_UI
+// IF DEFINED: enable and extended CLI with a longer input buffer for example.
+#undef ENABLE_EXTENDED_CLI
+// IF DEFINED: minimise boot effort and energy eg for intermittently-powered energy-harvesting applications.  ***
+#undef ENABLE_MIN_ENERGY_BOOT
+// IF DEFINED: enable a 'null' radio module; without this unit is stand-alone.
+#define ENABLE_RADIO_NULL
+// Secondary radio
+#undef ENABLE_RADIO_RFM23B
+#undef ENABLE_RADIO_PRIMARY_RFM23B
+// IF DEFINED: enable a secondary (typically WAN-relay) radio module.
+#define ENABLE_RADIO_SECONDARY_MODULE
+#define ENABLE_RADIO_RN2483   // Enable RN2483
+//#define RADIO_PRIMARY_RN2483 // Must be secondary to avoid sending preamble etc
+#define ENABLE_RADIO_SECONDARY_RN2483
+//#define ENABLE_FREQUENT_STATS_TX
+#define ENABLE_JSON_STATS_LEN_CAP 34 // Should allow LoRa SF12 and 4 min TX intervals.
+// Define voice module
+//#define ENABLE_VOICE_SENSOR
+//#define ENABLE_OCCUPANCY_DETECTION_FROM_VOICE
+//#define ENABLE_VOICE_STATS
+#endif
 
 #endif
 
