58f474ebbd00bb165a3cb3dcc1e28d46730151ec
denzo <denzo@denzo-mint>
2016-04-05 16:13:48 +0100

    Started REV11 secure stats hub config

--- content/OTRadioLink/OTV0p2_CONFIG_REV11.h
+++ content/OTRadioLink/OTV0p2_CONFIG_REV11.h
@@ -86,6 +86,52 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 #undef ENABLE_FS20_NATIVE_AND_BINARY_STATS_RX
 #endif
 
+#ifdef CONFIG_REV11_SECURE_STATSHUB // REV11 stats hub with authentication
+#define V0p2_REV 11
+// IF DEFINED: enable use of on-board SHT21 RH and temp sensor (in lieu of TMP112).
+#define ENABLE_PRIMARY_TEMP_SENSOR_SHT21
+// Using RoHS-compliant phototransistor in place of LDR.
+#define ENABLE_AMBIENT_LIGHT_SENSOR_PHOTOTRANS_TEPT4400
+// IF DEFINED: use the temperature-setting potentiometer/dial if present.
+#undef ENABLE_TEMP_POT_IF_PRESENT
+// IF DEFINED: basic FROST/WARM temperatures are settable.
+#undef ENABLE_SETTABLE_TARGET_TEMPERATURES
+// IF DEFINED: there is run-time help available for the CLI.
+#undef ENABLE_CLI_HELP
+// IF DEFINED: this unit will act as a thermostat controlling a local TRV (and calling for heat from the boiler), else is a sensor/hub unit.
+#undef ENABLE_LOCAL_TRV
+// IF DEFINED: this unit controls a valve, but provides slave valve control only.
+#undef ENABLE_SLAVE_TRV
+// IF DEFINED: (default) forced always-on radio listen/RX, eg not requiring setup to explicitly enable.
+#define ENABLE_DEFAULT_ALWAYS_RX
+// IF DEFINED: this unit can act as boiler-control hub listening to remote thermostats, possibly in addition to controlling a local TRV.
+#undef ENABLE_BOILER_HUB
+// IF DEFINED: allow RX of stats frames.
+#define ENABLE_STATS_RX
+// IF DEFINED: allow TX of stats frames.
+#undef ENABLE_STATS_TX
+// IF DEFINED: allow JSON stats frames alongside binary ones.
+#undef ENABLE_JSON_OUTPUT
+// IF DEFINED: enable periodic secure beacon broadcast.
+#undef ENABLE_SECURE_RADIO_BEACON
+// IF DEFINED: allow binary stats to be TXed.
+#undef ENABLE_BINARY_STATS_TX
+// IF DEFINED: enable support for fast (>50kbps) packet-handling carrier (leading length byte).
+#define ENABLE_FAST_FRAMED_CARRIER_SUPPORT
+// IF DEFINED: enable support for FS20 carrier for RX or TX.
+#undef ENABLE_FS20_CARRIER_SUPPORT
+// IF DEFINED: use FHT8V wireless radio module/valve.
+#undef ENABLE_FHT8VSIMPLE
+// IF DEFINED: enable support for FS20 carrier for TX specifically (to allow RX-only).
+#undef ENABLE_FS20_CARRIER_SUPPORT_TX
+// IF DEFINED: enable support for FS20 encoding/decoding, eg to send to FHT8V.
+#undef ENABLE_FS20_ENCODING_SUPPORT
+// IF DEFINED: enable OpenTRV secure frame encoding/decoding (as of 2015/12).
+#define ENABLE_OTSECUREFRAME_ENCODING_SUPPORT
+// IF DEFINED: allow non-secure OpenTRV secure frame RX (as of 2015/12): DISABLED BY DEFAULT.
+#undef ENABLE_OTSECUREFRAME_INSECURE_RX_PERMITTED
+#endif
+
 
 #endif
 
