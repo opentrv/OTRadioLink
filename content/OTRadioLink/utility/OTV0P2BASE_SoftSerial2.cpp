129bd40a9406ab2c02d0c6dd7a888ddb38227766
Denzo77 <denzo1@gmail.com>
2016-04-18 15:33:56 +0100

    more implementation

--- content/OTRadioLink/utility/OTV0P2BASE_SoftSerial2.cpp
+++ content/OTRadioLink/utility/OTV0P2BASE_SoftSerial2.cpp
@@ -9,43 +9,99 @@
 
 namespace OTV0P2BASE
 {
-
+/**
+ * @brief   Constructor for OTSoftSerial2
+ * @param   rxPin: Pin to receive from.
+ * @param   txPin: Pin to send from.
+ */
 OTSoftSerial2::OTSoftSerial2(uint8_t _rxPin, uint8_t _txPin): rxPin(_rxPin), txPin(_txPin)
 {
-
+    halfDelay = 0;
+    fullDelay = 0;
+    rxBufferHead = 0;
+    rxBufferTail = 0;
+    memset(rxBuffer, 0, sizeof(rxBuffer));
 }
+/**
+ * @brief   Initialises OTSoftSerial2 and sets up pins.
+ * @param   speed: The baud to listen at.
+ * @fixme   Long is excessive
+ */
+void OTSoftSerial2::begin(unsigned long speed) {
+    // Set delays
+    uint16_t bitCycles = (F_CPU/4) / speed;
+    fullDelay = bitCycles;
+    halfDelay = bitCycles/2;
 
-void OTSoftSerial2::begin(long speed) {
+    // Set pins for UART
+    pinMode(rxPin, INPUT_PULLUP);
+    pinMode(txPin, OUTPUT);
+    fastDigitalWrite(txPin, HIGH);
 }
-
-bool OTSoftSerial2::listen() {
-}
-
+/**
+ * @brief   Disables serial and releases pins.
+ * @todo    What state do we want to leave these in?
+ * @todo    Reset read buffer?
+ */
 void OTSoftSerial2::end() {
+    pinMode(txPin, INPUT_PULLUP);
 }
-
-bool OTSoftSerial2::isListening() {
-}
-
-bool OTSoftSerial2::stopListening() {
-}
-
-bool OTSoftSerial2::overflow() {
-}
-
-int OTSoftSerial2::peek() {
-}
-
+/**
+ * @brief   Write a byte to serial as a binary value.
+ * @param   byte: Byte to write.
+ * @retval  Number of bytes written.
+ */
 size_t OTSoftSerial2::write(uint8_t byte) {
-}
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+        uint8_t mask = 0x01;
+        uint8_t c = byte;
 
+        // Send start bit
+        fastDigitalWrite(txPin, LOW);
+        _delay_x4cycles(fullDelay);
+
+        // send byte. Loops until mask overflows back to 0
+        while(mask != 0) {
+            fastDigitalWrite(txPin, mask & c);
+            _delay_x4cycles(fullDelay);
+            mask = mask << 1;    // bit shift to next value
+        }
+
+        // send stop bit
+        fastDigitalWrite(txPin, HIGH);
+        _delay_x4cycles(fullDelay);
+
+        return 1;
+    }
+}
+/**
+ * @brief   Read next character in the input buffer without removing it.
+ * @retval  Next character in input buffer.
+ */
+int OTSoftSerial2::peek() {
+    if (rxBufferHead == rxBufferTail) return -1;
+    else return rxBuffer[rxBufferTail];
+}
+/**
+ * @brief   Reads a byte from the serial and removes it from the buffer.
+ * @retval  Next character in input buffer.
+ */
 int OTSoftSerial2::read() {
+    if (rxBuffer == rxBufferTail) return -1;
+    else {
+        uint8_t c = rxBuffer[rxBufferTail];
+        rxBufferTail = (rxBufferTail + 1) % sizeof(rxBuffer);
+        return c;
+    }
 }
-
+/**
+ * @brief   Get the number of bytes available to read in the input buffer.
+ * @retval  The number of bytes available.
+ */
 int OTSoftSerial2::available() {
-}
-
-void OTSoftSerial2::flush() {
+    uint8_t available = sizeof(rxBuffer) + rxBufferHead - rxBufferTail; // Extra addition keeps calculation positive. Probably unnecessary?
+    return (available % sizeof(rxBuffer));
 }
 
 }
--- content/OTRadioLink/utility/OTV0P2BASE_SoftSerial2.h
+++ content/OTRadioLink/utility/OTV0P2BASE_SoftSerial2.h
@@ -27,6 +27,8 @@ Author(s) / Copyright (s): Deniz Erbilgin 2016
 namespace OTV0P2BASE
 {
 
+static const uint8_t OTSOFTSERIAL2_BUFFER_SIZE = 32;
+
 /**
  * @class   OTSoftSerial2
  * @brief   Software serial with optional blocking read and settable interrupt pins.
@@ -34,28 +36,65 @@ namespace OTV0P2BASE
  */
 class OTSoftSerial2 : public Stream
 {
-private:
-    static const uint8_t rxPin;
-    static const uint8_t txPin;
+protected:
+    const uint8_t rxPin;
+    const uint8_t txPin;
+    uint8_t fullDelay;
+    uint8_t halfDelay;
+    volatile uint8_t rxBufferHead;
+    volatile uint8_t rxBufferTail;
+    uint8_t rxBuffer[OTSOFTSERIAL2_BUFFER_SIZE];
 
 public:
+    /**
+     * @brief   Constructor for OTSoftSerial2
+     * @param   rxPin: Pin to receive from.
+     * @param   txPin: Pin to send from.
+     */
     OTSoftSerial2(uint8_t rxPin, uint8_t txPin);
-
-    void begin(long speed);
-    bool listen();
+//    ~OTSoftSerial2() {};    // TODO do I delete this?
+    /**
+     * @brief   Initialises OTSoftSerial2 and sets up pins.
+     * @param   speed: The baud to listen at.
+     * @fixme   Long is excessive
+     */
+    void begin(unsigned long speed);
+    /**
+     * @brief   Disables serial and releases pins.
+     */
     void end();
-    bool isListening();
-    bool stopListening();
-    bool overflow();
-    int peek();
-
+    /**
+     * @brief   Write a byte to serial as a binary value.
+     * @param   byte: Byte to write.
+     * @retval  Number of bytes written.
+     */
     virtual size_t write(uint8_t byte);
+    /**
+     * @brief   Read next character in the input buffer without removing it.
+     * @retval  Next character in input buffer.
+     */
+    int peek();
+    /**
+     * @brief   Reads a byte from the serial and removes it from the buffer.
+     * @retval  Next character in input buffer.
+     */
     virtual int read();
+    /**
+     * @brief   Get the number of bytes available to read in the input buffer.
+     * @retval  The number of bytes available.
+     */
     virtual int available();
-    virtual void flush();
+    /**
+     * @brief   Waits for transmission of outgoing serial data to complete.
+     *          This is not used for OTSoftSerial2 as all writes are synchronous.
+     */
+    virtual void flush() {};
+    /**
+     * @brief   Check if serial port is ready for use.
+     * @todo    Implement the time checks using this?
+     */
     operator bool() { return true; }
-
-    using Print::write;
+    using Print::write; // write(str) and write(buf, size) from Print
 };
 
 
