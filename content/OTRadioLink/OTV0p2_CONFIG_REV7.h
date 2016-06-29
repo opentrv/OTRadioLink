dc29e83e634941544969868c1ed137f56470d95a
Deniz Erbilgin <denzo@denzo-desktop>
2016-06-29 13:39:22 +0100

    Added minimal config for key testing

--- content/OTRadioLink/OTV0p2_CONFIG_REV7.h
+++ content/OTRadioLink/OTV0p2_CONFIG_REV7.h
@@ -212,6 +212,109 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 #define ENABLE_LOCAL_TRV
 #endif
 
+#ifdef CONFIG_REV7_EEPROM_TEST // Config for isolating key loss.
+// GENERIC
+// Revision REV7 of V0.2 board, all-in-one valve unit with local motor drive.
+// Does not ever need to act as a boiler hub nor to receive stats.
+// GENERIC
+#define V0p2_REV 7
+// IF DEFINED: simplified mode button behaviour: tapping button invokes BAKE, not mode cycling.
+//#define ENABLE_SIMPLIFIED_MODE_BAKE
+#undef ENABLE_SIMPLIFIED_MODE_BAKE
+// IF DEFINED: fast temp pot/dial sampling to partly compensate for less good mechanics (at some energy cost).
+//#define ENABLE_FAST_TEMP_POT_SAMPLING
+#undef ENABLE_FAST_TEMP_POT_SAMPLING
+// IF DEFINED: initial direct motor drive design.
+//#define ENABLE_V1_DIRECT_MOTOR_DRIVE
+#undef ENABLE_V1_DIRECT_MOTOR_DRIVE
+// IF DEFINED: enable use of on-board SHT21 RH and temp sensor (in lieu of TMP112).
+#define ENABLE_PRIMARY_TEMP_SENSOR_SHT21
+// Using RoHS-compliant phototransistor in place of LDR.
+#define ENABLE_AMBIENT_LIGHT_SENSOR_PHOTOTRANS_TEPT4400
+// IF DEFINED: detect occupancy based on relative humidity, if available.
+// DHD20160101: seems to still be set off spuriously by fast drop in temp when rad turns off (TODO-696).
+#undef ENABLE_OCCUPANCY_DETECTION_FROM_RH
+// IF UNDEFINED: this unit cannot act as boiler-control hub listening to remote thermostats, possibly in addition to controlling a local TRV.
+#undef ENABLE_BOILER_HUB
+// IF DEFINED: allow TX of stats frames.
+#define ENABLE_STATS_TX
+// IF UNDEFINED: do not allow RX of stats frames.
+#undef ENABLE_STATS_RX
+// IF DEFINED: allow radio listen/RX.
+#undef ENABLE_RADIO_RX
+// IF DEFINED: forced always-on radio listen/RX, eg not requiring setup to explicitly enable.
+#undef ENABLE_DEFAULT_ALWAYS_RX
+// IF DEFINED: allow JSON stats frames.
+#define ENABLE_JSON_OUTPUT
+// IF DEFINED: allow binary stats to be TXed.
+#undef ENABLE_BINARY_STATS_TX
+// IF DEFINED: enable support for FS20 carrier for RX of raw FS20 and piggybacked binary (non-JSON) stats.
+#undef ENABLE_FS20_NATIVE_AND_BINARY_STATS_RX
+// IF DEFINED: this unit will act as a thermostat controlling a local TRV (and calling for heat from the boiler), else is a sensor/hub unit.
+//#define ENABLE_LOCAL_TRV
+#undef ENABLE_LOCAL_TRV
+// IF DEFINED: this unit supports CLI over the USB/serial connection, eg for run-time reconfig.
+#define ENABLE_CLI
+// IF DEFINED: there is run-time help available for the CLI.
+#undef ENABLE_CLI_HELP
+// IF DEFINED: enable a full OpenTRV CLI.
+#define ENABLE_FULL_OT_CLI
+// IF DEFINED: enable and extended CLI with a longer input buffer for example.
+#undef ENABLE_EXTENDED_CLI
+// IF DEFINED: enable a full OpenTRV UI with normal LEDs etc.
+//#define ENABLE_FULL_OT_UI
+#undef ENABLE_FULL_OT_UI
+// IF DEFINED: enable use of second UI LED if available.
+#undef ENABLE_UI_LED_2_IF_AVAILABLE
+// IF DEFINED: reverse DORM1 motor with respect to very first samples.
+//#define ENABLE_DORM1_MOTOR_REVERSED
+#undef ENABLE_DORM1_MOTOR_REVERSED
+
+// Revision REV7 of V0.2 board, all-in-one valve unit with local motor drive.
+// Does not ever need to act as a boiler hub nor to receive stats.
+// Although LEARN buttons are provided, by default they are disabled as is the scheduler.
+// Fast temp dial sampling is forced on to help compensate for mechanical slop in early devices.
+// Delayed activation is enabled.  (TODO-786)
+// IF DEFINED: try to trim memory (primarily RAM, also code/Flash) space used.
+//#define ENABLE_TRIMMED_MEMORY
+#undef ENABLE_TRIMMED_MEMORY
+// IF DEFINED: try to trim bandwidth as may be especially expensive/scarce.
+#undef ENABLE_TRIMMED_BANDWIDTH
+// IF DEFINED: support one on and one off time per day (possibly in conjunction with 'learn' button).
+#undef ENABLE_SINGLETON_SCHEDULE
+// IF DEFINED: use active-low LEARN button(s).  Needs ENABLE_SINGLETON_SCHEDULE.
+#undef ENABLE_LEARN_BUTTON // OPTIONAL ON V0.09 PCB1
+// IF DEFINED: allow periodic machine- and human- readable status report to serial, starting with "=".
+#undef ENABLE_SERIAL_STATUS_REPORT
+// IF DEFINED: allow binary stats to be TXed.
+#undef ENABLE_BINARY_STATS_TX
+// IF DEFINED: enable support for FS20 carrier for RX or TX.
+#undef ENABLE_FS20_CARRIER_SUPPORT
+// IF DEFINED: use FHT8V wireless radio module/valve.
+#undef ENABLE_FHT8VSIMPLE
+// IF DEFINED: enable support for FS20 carrier for TX specifically (to allow RX-only).
+#undef ENABLE_FS20_CARRIER_SUPPORT_TX
+// IF DEFINED: enable raw preamble injection/framing eg for FS20 over RFM23B.
+#undef ENABLE_RFM23B_FS20_RAW_PREAMBLE
+// IF DEFINED: enable support for FS20 encoding/decoding, eg to send to FHT8V.
+#undef ENABLE_FS20_ENCODING_SUPPORT
+// IF DEFINED: enable periodic secure beacon broadcast.
+#undef ENABLE_SECURE_RADIO_BEACON
+// IF DEFINED: allow non-secure OpenTRV secure frame RX (as of 2015/12): DISABLED BY DEFAULT.
+#undef ENABLE_OTSECUREFRAME_INSECURE_RX_PERMITTED
+// IF DEFINED: enable support for fast (>50kbps) packet-handling carrier (leading length byte).
+#define ENABLE_FAST_FRAMED_CARRIER_SUPPORT
+// IF DEFINED: enable OpenTRV secure frame encoding/decoding (as of 2015/12).
+// DHD20160214: costs 5866 bytes to enable vs 3426 for FS20 support.
+#define ENABLE_OTSECUREFRAME_ENCODING_SUPPORT
+// IF DEFINED: always allow some kind of stats TX, whatever the privacy settings.
+// OK IN THIS CASE BECAUSE ALL COMMS SECURE.
+#define ENABLE_ALWAYS_TX_ALL_STATS
+// IF DEFINED: enable a CLI-settable setback lockout (hours/days) to establish a baseline before engaging energy saving setbacks.
+//#define ENABLE_SETBACK_LOCKOUT_COUNTDOWN
+#undef ENABLE_SETBACK_LOCKOUT_COUNTDOWN
+
+#endif
 
 #endif
 
