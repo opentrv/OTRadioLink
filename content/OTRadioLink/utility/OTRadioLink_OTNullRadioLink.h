80f64e9eef35ca87e1bce50b80dc5cb18b256de5
DamonHD <d@hd.org>
2016-09-24 16:30:00 +0100

    tidyup

--- content/OTRadioLink/utility/OTRadioLink_OTNullRadioLink.h
+++ content/OTRadioLink/utility/OTRadioLink_OTNullRadioLink.h
@@ -14,6 +14,7 @@ specific language governing permissions and limitations
 under the Licence.
 
 Author(s) / Copyright (s): Deniz Erbilgin 2015
+                           Damon Hart-Davis 2016
 */
 
 #ifndef OTRADIOLINK_OTNULLRADIOLINK_H_
@@ -25,22 +26,22 @@ Author(s) / Copyright (s): Deniz Erbilgin 2015
 
 namespace OTRadioLink{
 /**
- * @brief	This is a skeleton class that extends OTRadiolink and does nothing
+ * @brief    This is a skeleton class that extends OTRadiolink and does nothing
  */
 class OTNullRadioLink : public OTRadioLink::OTRadioLink
 {
 /****************** Interface *******************/
 public:
-	OTNullRadioLink();
-	bool begin() {return (true);};
-	void getCapacity(uint8_t &queueRXMsgsMin, uint8_t &maxRXMsgLen, uint8_t &maxTXMsgLen) const;
-	uint8_t getRXMsgsQueued() const;
-	const volatile uint8_t *peekRXMsg() const;
-	void removeRXMsg();
-	// Should always be sent null terminated strings
-	bool sendRaw(const uint8_t *buf, uint8_t buflen, int8_t channel = 0, TXpower power = TXnormal, bool listenAfter = false);
+    OTNullRadioLink();
+    bool begin() { return(true); };
+    void getCapacity(uint8_t &queueRXMsgsMin, uint8_t &maxRXMsgLen, uint8_t &maxTXMsgLen) const;
+    uint8_t getRXMsgsQueued() const;
+    const volatile uint8_t *peekRXMsg() const;
+    void removeRXMsg();
+    // Should always be sent null-terminated strings.
+    bool sendRaw(const uint8_t *buf, uint8_t buflen, int8_t channel = 0, TXpower power = TXnormal, bool listenAfter = false);
 private:
-	void _dolisten() {};
+    void _dolisten() {};
 };
 
 }  //OTNullRadioLink
