fe7fe4292fcbcabb96ea919063ec384f1cbc32d5
denzo <denzo@denzo-desktop>
2016-10-11 16:02:36 +0100

    Updated licenses

--- content/OTRadioLink/utility/OTRadValve_ValveMotorBase.h
+++ content/OTRadioLink/utility/OTRadValve_ValveMotorBase.h
@@ -1,12 +1,25 @@
 /*
- * OTRadValve_ValveMotorBase.h
- *
- *  Created on: 11 Oct 2016
- *      Author: denzo
- */
+The OpenTRV project licenses this file to you
+under the Apache Licence, Version 2.0 (the "Licence");
+you may not use this file except in compliance
+with the Licence. You may obtain a copy of the Licence at
 
-#ifndef CONTENT_OTRADIOLINK_UTILITY_OTRADVALVE_VALVEMOTORBASE_H_
-#define CONTENT_OTRADIOLINK_UTILITY_OTRADVALVE_VALVEMOTORBASE_H_
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the Licence is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the Licence for the
+specific language governing permissions and limitations
+under the Licence.
+
+Author(s) / Copyright (s): Damon Hart-Davis 2015--2016
+                            Deniz Erbilgin 2016
+*/
+
+
+#ifndef ARDUINO_LIB_OTRADVALVE_VALVEMOTORBASE_H_
+#define ARDUINO_LIB_OTRADVALVE_VALVEMOTORBASE_H_
 
 
 #include <stdint.h>
@@ -346,4 +359,4 @@ class CurrentSenseValveMotorDirect : public OTRadValve::HardwareMotorDriverInter
 
 }
 
-#endif /* CONTENT_OTRADIOLINK_UTILITY_OTRADVALVE_VALVEMOTORBASE_H_ */
+#endif /* ARDUINO_LIB_OTRADVALVE_VALVEMOTORBASE_H_ */
