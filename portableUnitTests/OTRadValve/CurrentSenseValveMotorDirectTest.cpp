cec4733b3c5a957590aa54a063cc9121eec8e626
DamonHD <d@hd.org>
2016-10-18 15:27:43 +0100

    TODO-1003: WIP

---
+++ portableUnitTests/OTRadValve/CurrentSenseValveMotorDirectTest.cpp
@@ -0,0 +1,39 @@
+/*
+The OpenTRV project licenses this file to you
+under the Apache Licence, Version 2.0 (the "Licence");
+you may not use this file except in compliance
+with the Licence. You may obtain a copy of the Licence at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the Licence is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the Licence for the
+specific language governing permissions and limitations
+under the Licence.
+
+Author(s) / Copyright (s): Damon Hart-Davis 2016
+*/
+
+/*
+ * OTRadValve CurrentSenseValveMotorDirect tests.
+ */
+
+#include <gtest/gtest.h>
+#include <cstdint>
+#include <cstdio>
+#include <OTV0P2BASE_QuickPRNG.h>
+
+#include "OTRadValve_AbstractRadValve.h"
+#include "OTRadValve_ValveMotorDirectV1.h"
+
+
+// Test for XXX
+// In particular test the logic in ModelledRadValveState for starting from extreme positions.
+//
+// Adapted 2016/10/16 from test_VALVEMODEL.ino testMRVSExtremes().
+TEST(CurrentSenseValveMotorDirect,BLAH)
+{
+
+}
