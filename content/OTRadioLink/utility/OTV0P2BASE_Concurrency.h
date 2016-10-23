2d7785bc4daf124e3a4550fb585d256c4d160b5b
DamonHD <d@hd.org>
2016-10-23 15:05:34 +0100

    TODO-970: Atomic_UInt8T: WIP

--- content/OTRadioLink/utility/OTV0P2BASE_Concurrency.h
+++ content/OTRadioLink/utility/OTV0P2BASE_Concurrency.h
@@ -17,14 +17,14 @@ Author(s) / Copyright (s): Damon Hart-Davis 2016
 */
 
 /*
- Portable concurrency/atomicity support that should work for small MCUs.
+ Portable concurrency/atomicity support that should work for small MCUs and bigger platforms.
 
  Has some fairly ugly stuff in the header to allow
  fast, efficient, hardware-specific support.
 
  To some extent modelled on Java and C++ support,
  eg java.util.concurrent.atomic.AtomicReference and std::atomic.
- Actual MCU implementations are likely to be restricted subsets and hand-optimised.
+ Actual MCU implementations are likely to be heavily restricted subsets and hand-optimised.
 
  See: https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/atomic/AtomicReference.html
  See: http://en.cppreference.com/w/cpp/atomic/atomic
@@ -33,13 +33,34 @@ Author(s) / Copyright (s): Damon Hart-Davis 2016
 #ifndef OTV0P2BASE_CONCURRENCY_H
 #define OTV0P2BASE_CONCURRENCY_H
 
+#include <stdint.h>
+#include <OTV0p2Base.h>
+
+#ifndef ARDUINO
+// Non-Arduino platforms may have grown-up concurrency support.
 #include <atomic>
+#define OTV0P2BASE_PLATFORM_HAS_atomic
+#endif
 
 namespace OTV0P2BASE
 {
 
+// A uint8_t atomic value.
+#ifdef OTV0P2BASE_PLATFORM_HAS_atomic
     // Default is to use the std::atomic where it exists, eg for hosted test cases.
     typedef std::atomic<std::uint8_t> Atomic_UInt8T;
+#elif defined(ARDUINO_ARCH_AVR)
+    struct Atomic_UInt8T
+        {
+        public:
+            // Direct access to value.
+            // Use sparingly, eg where concurrency is not an issue on an MCU, eg with interrupts locked out.
+            uint8_t value;
+
+            Atomic_UInt8T() { }
+            Atomic_UInt8T(uint8_t v) : value(v) { }
+        };
+#endif // OTV0P2BASE_PLATFORM_HAS_atomic ... defined(ARDUINO_ARCH_AVR)
 
 //class Atomic
 //  {
