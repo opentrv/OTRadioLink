91ddbf40539521253db3477293c1a7a59b025178
DamonHD <d@hd.org>
2016-10-29 21:18:49 +0100

    TODO-970: more JSON tests

--- portableUnitTests/OTV0p2Base/JSONStatsTest.cpp
+++ portableUnitTests/OTV0p2Base/JSONStatsTest.cpp
@@ -68,70 +68,70 @@ TEST(JSONStats,JSONStats)
     EXPECT_EQ(1, ss1.size());
     EXPECT_EQ(22, ss1.writeJSON((uint8_t*)buf, sizeof(buf), 0, OTV0P2BASE::randRNG8NextBoolean()));
     EXPECT_STREQ(buf, "{\"@\":\"1234\",\"f1\":-111}");
+
+    EXPECT_TRUE(OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
 }
 
 
-#if 0
-
 // Test handling of JSON messages for transmission and reception.
 // Includes bit-twiddling, CRC computation, and other error checking.
-static void testJSONForTX()
+//
+// Imported 2016/10/29 from Unit_Tests.cpp testJSONForTX().
+TEST(JSONStats,JSONForTX)
   {
-#if defined(ENABLE_JSON_OUTPUT)
-  DEBUG_SERIAL_PRINTLN_FLASHSTRING("JSONForTX");
-  char buf[MSG_JSON_MAX_LENGTH + 2]; // Allow for trailing '\0' or CRC + 0xff terminator.
+  char buf[OTV0P2BASE::MSG_JSON_MAX_LENGTH + 2]; // Allow for trailing '\0' or CRC + 0xff terminator.
   // Clear the buffer.
   memset(buf, 0, sizeof(buf));
   // Fail sanity check on a completely empty buffer (zero-length string).
-  AssertIsTrue(!quickValidateRawSimpleJSONMessage(buf));
+  EXPECT_TRUE(!OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   // Fail sanity check on a few initially-plausible length-1 values.
   buf[0] = '{';
-  AssertIsTrue(!quickValidateRawSimpleJSONMessage(buf));
+  EXPECT_TRUE(!OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   buf[0] = '}';
-  AssertIsTrue(!quickValidateRawSimpleJSONMessage(buf));
+  EXPECT_TRUE(!OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   buf[0] = '[';
-  AssertIsTrue(!quickValidateRawSimpleJSONMessage(buf));
+  EXPECT_TRUE(!OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   buf[0] = ']';
-  AssertIsTrue(!quickValidateRawSimpleJSONMessage(buf));
+  EXPECT_TRUE(!OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   buf[0] = ' ';
-  AssertIsTrue(!quickValidateRawSimpleJSONMessage(buf));
+  EXPECT_TRUE(!OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   // Fail sanity check with already-adjusted (minimal) nessage.
   buf[0] = '{';
   buf[1] = ('}' | 0x80);
-  AssertIsTrue(!quickValidateRawSimpleJSONMessage(buf));
+  EXPECT_TRUE(!OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   // Minimal correct messaage should pass.
   buf[0] = '{';
   buf[1] = '}';
-  AssertIsTrue(quickValidateRawSimpleJSONMessage(buf));
+  EXPECT_TRUE(OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   // Try a longer valid trivial message.
-  strcpy_P(buf, (const char PROGMEM *)F("{  }"));
-  AssertIsTrue(quickValidateRawSimpleJSONMessage(buf));
+  strcpy(buf, "{  }");
+  EXPECT_TRUE(OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   // Invalidate it with a non-printable char and check that it is rejected.
   buf[2] = '\1';
-  AssertIsTrue(!quickValidateRawSimpleJSONMessage(buf));
+  EXPECT_TRUE(!OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   // Try a longer valid non-trivial message.
-  __FlashStringHelper const* longJSONMsg1 = F("{\"@\":\"cdfb\",\"T|C16\":299,\"H|%\":83,\"L\":255,\"B|cV\":256}");
+  const char * longJSONMsg1 = "{\"@\":\"cdfb\",\"T|C16\":299,\"H|%\":83,\"L\":255,\"B|cV\":256}";
   memset(buf, 0, sizeof(buf));
-  strcpy_P(buf, (const char PROGMEM *)longJSONMsg1);
-  AssertIsTrue(quickValidateRawSimpleJSONMessage(buf));
+  strcpy(buf, longJSONMsg1);
+  EXPECT_TRUE(OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   // Invalidate it with a high-bit set and check that it is rejected.
   buf[5] |= 0x80;
-  AssertIsTrue(!quickValidateRawSimpleJSONMessage(buf));
+  EXPECT_TRUE(!OTV0P2BASE::quickValidateRawSimpleJSONMessage(buf));
   // CRC fun!
   memset(buf, 0, sizeof(buf));
   buf[0] = '{';
   buf[1] = '}';
-  const uint8_t crc1 = adjustJSONMsgForTXAndComputeCRC(buf);
+  const uint8_t crc1 = OTV0P2BASE::adjustJSONMsgForTXAndComputeCRC(buf);
   // Check that top bit is not set (ie CRC was computed OK).
-  AssertIsTrueWithErr(!(crc1 & 0x80), crc1);
+  EXPECT_TRUE(!(crc1 & 0x80));
   // Check for expected CRC value.
-  AssertIsTrueWithErr((0x38 == crc1), crc1);
+  EXPECT_TRUE(0x38 == crc1);
   // Check that initial part unaltered.
-  AssertIsTrueWithErr(('{' == buf[0]), buf[0]);
+  EXPECT_TRUE('{' == buf[0]);
   // Check that top bit has been set in trailing brace.
-  AssertIsTrueWithErr(((char)('}' | 0x80) == buf[1]), buf[1]);
+  EXPECT_TRUE((char)('}' | 0x80) == buf[1]);
   // Check that trailing '\0' still present.
-  AssertIsTrueWithErr((0 == buf[2]), buf[2]);
+  EXPECT_TRUE(0 == buf[2]);
   // Check that TX-format can be converted for RX.
   buf[2] = crc1;
   buf[3] = 0xff; // As for normal TX...
@@ -142,13 +142,13 @@ static void testJSONForTX()
 //  AssertIsTrue(quickValidateRawSimpleJSONMessage(buf));
   // Now a longer message...
   memset(buf, 0, sizeof(buf));
-  strcpy_P(buf, (const char PROGMEM *)longJSONMsg1);
-  const int8_t l2o = strlen(buf);
-  const uint8_t crc2 = adjustJSONMsgForTXAndComputeCRC(buf);
+  strcpy(buf, longJSONMsg1);
+//  const int l2o = strlen(buf);
+  const uint8_t crc2 = OTV0P2BASE::adjustJSONMsgForTXAndComputeCRC(buf);
   // Check that top bit is not set (ie CRC was computed OK).
-  AssertIsTrueWithErr(!(crc2 & 0x80), crc2);
+  EXPECT_TRUE(!(crc2 & 0x80));
   // Check for expected CRC value.
-  AssertIsTrueWithErr((0x77 == crc2), crc2);
+  EXPECT_TRUE(0x77 == crc2);
 // FIXME
 //  // Check that TX-format can be converted for RX.
 //  buf[l2o] = crc2;
@@ -157,7 +157,4 @@ static void testJSONForTX()
 //  const int8_t l2 = adjustJSONMsgForRXAndCheckCRC(buf, sizeof(buf));
 //  AssertIsTrueWithErr(l2o == l2, l2);
 //  AssertIsTrue(quickValidateRawSimpleJSONMessage(buf));
-#endif
   }
-
-#endif // 0
