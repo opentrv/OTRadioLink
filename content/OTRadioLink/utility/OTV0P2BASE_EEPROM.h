390e384eab9044036fc5332524d2723d1b0f350a
DamonHD <d@hd.org>
2016-11-12 12:18:37 +0000

    TODO-970: comments and clarity

--- content/OTRadioLink/utility/OTV0P2BASE_EEPROM.h
+++ content/OTRadioLink/utility/OTV0P2BASE_EEPROM.h
@@ -77,10 +77,10 @@ class NVByHourByteStatsBase
     //   * maxBytesToErase limit the number of bytes erased to this; strictly positive, else 0 to allow 65536
     // Returns true if finished with all bytes erased.
     //
-    // Till not get during most executions,
-    // is not performance-critical (though must not cause overruns)
-    // So may be usefully marked as "cold" or "optimise for space"
-    // for many implementations/compilers.
+    // Till not get called during most system executions,
+    // and is not performance-critical (though must not cause overruns),
+    // so may be usefully marked as "cold" or "optimise for space"
+    // for most implementations/compilers.
     virtual bool zapStats(uint16_t maxBytesToErase = 0) = 0;
 
     // Get raw stats value for specified hour [0,23]/current/next from stats set N from non-volatile (EEPROM) store.
