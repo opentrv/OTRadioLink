69396bda20bfbbc8531aa9c9b41fe316deab4914
DamonHD <d@hd.org>
2017-01-14 15:43:30 +0000

    improved comments
    [skip ci]

--- portableUnitTests/OTRadValve/ModelledRadValveThemalModelTest.cpp
+++ portableUnitTests/OTRadValve/ModelledRadValveThemalModelTest.cpp
@@ -68,4 +68,14 @@ TEST(ModelledRadValveThermalModel,basic)
 
 Test for sticky / jammed / closed value calling for heat in stable temp room running boiler continually: TODO-1096
 
+Test for difficult cases such as:
+  * DHW (esp needing glacial as per Bo).
+  * All-in-one (TRV1.x) on flow end of rad ver slow to heat up
+    so valve must let whole rad get warm to let room get warm
+    (eg see 1g 2017/01/14 12:00 to 15:00 om tag 20170114-responsiveness).
+  * All-in-one (TRV1.x) on rad with very poor air circulation.
+  * Split unit (REV2+FHT8V style) with sensor close to and far from radiator.
+  * All-in-one or split unit in draughty room.
+  * All-in-one or split unit in room with door or window opened on cold day.
+
  */
