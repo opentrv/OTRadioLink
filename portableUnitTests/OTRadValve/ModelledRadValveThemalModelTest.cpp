39f0cbf75f6a23ada5a849cb817975ac3205af4f
Denzo77 <denzo77@users.noreply.github.com>
2017-02-07 15:08:58 +0000

    broken everything (tm)

--- portableUnitTests/OTRadValve/ModelledRadValveThemalModelTest.cpp
+++ portableUnitTests/OTRadValve/ModelledRadValveThemalModelTest.cpp
@@ -49,12 +49,70 @@ class ThermalModelBase
         // Simulated room temperature, internal.
         OTV0P2BASE::TemperatureC16Mock roomTemperatureInternal;
 
+        // Constants
+        constexpr float radiatorConductance = 1.0;
+        constexpr float wallConductance = 1.0;
+        constexpr float storageCapacitance = 1.0;
+        constexpr float storageConductance = 1.0;
+        constexpr float airCapacitance = 1.0;
+        float outsideTemp = 0.0;
+        float storedHeat = 0.0;
+
+        // Internal methods
+        /**
+         * @brief   Calculate heat input this interval by radiator.
+         * @note    Heat flow into the room is positive.
+         * @note    Assumes that radiator temperature (and therefore heat input):
+         *          - increases linearly.
+         *          - increases monotonically.
+         *          - Cannot be below air temperature (the radiator cannot sink heat).
+         */
+        float calcHeatFlowRad(const float airTemp, const uint8_t radValveOpenPC) {
+            // convert radValveOpenPC to radiator temp (badly)
+            const float radTemp = 2.0 * (float)radValveOpenPC - 80.0;
+            // Calculate heat transfer, making sure rad temp cannot go below air temperature.
+            return radTemp > airTemp ? ((radTemp - airTemp) * radiatorConductance) : airTemp;
+        }
+        /**
+         * @brief   Calculate heat input this interval through the walls.
+         * @note    Heat flow into the room is positive.
+         * @note    This is the heat loss, but is modelled as a heat input for consistency.
+         *          - Should return a negative value if outside temp is colder than inside temp.
+         */
+        float calcHeatFlowWalls(const float airTemp, const float outsideTemp) {
+            return ((outsideTemp - airTemp) * wallConductance);
+        }
+        /**
+         * @brief   Calculate the heat flow into the room from miscellaneous heat capacitances.
+         * @note    Intended to model the effects things within the room other than air
+         *          (e.g. walls, furniture) storing and releasing heat.
+         */
+        float calcHeatStored(const float airTemp, const float storedHeat) {
+            return (((storedHeat / storageCapacitance) - airTemp) * storageConductance);
+        }
+
     public:
         // Read-only view of simulated room temperature.
         const OTV0P2BASE::TemperatureC16Base &roomTemperature = roomTemperatureInternal;
 
         // Read-only view of simulated radiator valve.
         const OTRadValve::AbstractRadValve &radValve = radValveInternal;
+
+        // Calculate new temperature
+        int16_t calcNewAirTemperature() {
+            int16_t temperatureC16 = roomTemperatureInternal.read();
+            float temperature = ((float)temperatureC16) / 16.0;
+            float sumHeats = 0.0;
+            float deltaHeat = calcHeatStored(temperature, storedHeat);
+            storedHeat -= deltaHeat;  // all heat flows are positive into the air!
+            sumHeats += calcHeatFlowRad(temperature, radValveInternal.get());
+            sumHeats += calcHeatFlowWalls(temperature, outsideTemp);
+            sumHeats += deltaHeat;
+            temperature += (sumHeats * (1.0 / airCapacitance));
+            temperatureC16 = (int16_t)(temperature / 16.0);
+            roomTemperatureInternal.set(temperatureC16);
+            return temperatureC16;
+        }
     };
 
 
