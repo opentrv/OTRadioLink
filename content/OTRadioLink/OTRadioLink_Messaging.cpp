ff676704327805025f95c0ce4d0ddbf56f954b52
Denzo77 <denzo77@users.noreply.github.com>
2017-05-18 13:40:03 +0100

    Created new files for messaging

---
+++ content/OTRadioLink/OTRadioLink_Messaging.cpp
@@ -0,0 +1,10 @@
+/*
+ * OTRadioLink_Messaging.cpp
+ *
+ *  Created on: 18 May 2017
+ *      Author: denzo
+ */
+
+#include "OTRadioLink_Messaging.h"
+
+
---
+++ content/OTRadioLink/utility/OTRadioLink_Messaging.h
@@ -0,0 +1,15 @@
+/*
+ * OTRadioLink_Messagin.h
+ *
+ *  Created on: 18 May 2017
+ *      Author: denzo
+ */
+
+#ifndef UTILITY_OTRADIOLINK_MESSAGING_H_
+#define UTILITY_OTRADIOLINK_MESSAGING_H_
+
+
+
+
+
+#endif /* UTILITY_OTRADIOLINK_MESSAGING_H_ */
